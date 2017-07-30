ce019764a166a68864b9b1ffdd2e3d96e6873726
laidick <laidick@gmail.com>
2017-07-30 17:12:08 +0800

    test get temp

--- extension.js
+++ extension.js
@@ -8,9 +8,23 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    ext.get_temp = function(location, callback) {
+        // Make an AJAX call to the Open Weather Maps API
+        $.ajax({
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
+              dataType: 'jsonp',
+              success: function( weather_data ) {
+                  // Got the data - parse it and return the temperature
+                  temperature = weather_data['main']['temp'];
+                  callback(temperature);
+              }
+        });
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
+            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
         ]
     };
 
